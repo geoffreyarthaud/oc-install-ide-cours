061d70a436164ca759c3242bc60765c0d2015447
Geoffrey ARTHAUD <garthaud@gmail.com>
2019-07-08 16:17:24 +0200

    Corrections et modifs fonctionnelles

---
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.ideinstall;
+
+public class BasicExample {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0; i<3; i++) {
+			System.out.println("Hello bugs !");
+		}
+	}
+}
