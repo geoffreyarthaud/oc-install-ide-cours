6287f1709d609f2875d881ee5dc188677ee040be
HappyCodingBadger <laure.lamande@gmail.com>
2019-11-06 14:20:44 +0100

    correction erreur syntaxe et erreur execution

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,7 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int index;
+		int i;
 		for (i = 0; i < 3; i++) {
 			System.out.println("Hello bugs !");
 		}
