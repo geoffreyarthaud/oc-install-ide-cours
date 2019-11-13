00c2b5ab4f11d98a48593acd730fc332d5bfb61b
Votre nom ou pseudo <Votre@email.com>
2019-11-13 18:24:17 +0100

    Correction des erreurs de syntaxe et d'exécution.

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,7 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int index;
-		for (index = 0; index > 3; index++) {
+		for (index = 0; index > 4; index++) {
 			System.out.println("Hello bugs !");
 		}
 	}
