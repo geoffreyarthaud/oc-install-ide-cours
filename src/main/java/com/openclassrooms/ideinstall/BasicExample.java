4c688a052383673e16cf11b0c15dd3f577d112d8
Papoune <npapon@live.fr>
2019-11-17 11:25:41 +0100

    Correction du bug

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -2,10 +2,10 @@ package com.openclassrooms.ideinstall;
 
 public class BasicExample {
 
-	public static void main(String[] args) {
-		final int index;
-		for(i=0; i>=0; i++) {
-			System.out.println("Hello bugs !"):
-		}
-	}
+    public static void main( String[] args ) {
+        final int index;
+        for ( int i = 0; i <= 10; i++ ) {
+            System.out.println( "Hello bugs !" );
+        }
+    }
 }
