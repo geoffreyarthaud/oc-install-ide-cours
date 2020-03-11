d66d5ea35561d09b3d2cacd6e3a331091d7f4c8f
krind <krind@DESKTOP-L63L5LN>
2020-03-11 23:42:36 +0100

    Corrections des erreurs de syntaxe et d'exécution.

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,7 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 2; i++) {
 			System.out.println("Hello bugs !");
 		}
 	}
