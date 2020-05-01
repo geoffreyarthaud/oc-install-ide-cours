be1e7955001ebdc453082d83472756275ebaea62
christian soulagnet <christiansoulagnet@gmail.com>
2020-05-01 19:31:15 +0200

    correction des erreurs de synthaxe et des erreurs d'execution

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,9 +3,14 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		final int index;
-		for(i=0; i>=0; i++) {
-			System.out.println("Hello bugs !"):
+		
+		int index;
+		
+		for(index=0; index<=20; index++) {
+			System.out.println("Hello bugs => index  : " + index);
 		}
+		
+		
+		System.out.println("Fin de programme !!! ");
 	}
 }
