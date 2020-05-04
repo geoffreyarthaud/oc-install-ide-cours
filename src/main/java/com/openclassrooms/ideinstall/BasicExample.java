1d56364be54272af326f89d22566749545cb589b
athar hakim <hakim.athar.88@gmail.com>
2020-05-04 14:56:34 +0100

    ajouter boucle

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -7,5 +7,9 @@ public class BasicExample {
 		for(i=0; i<5; i++) {
 			System.out.println("Hello bugs !");
 		}
+		 int j;
+		for(j=0; j<5; j++) {
+			System.out.println("Hello world !");
+		}
 	}
 }
