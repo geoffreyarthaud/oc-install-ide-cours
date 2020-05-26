55990c67e4e50be802341f50ab5d4cfdab778171
aleth <aleth@212.201.83.108>
2020-05-26 21:40:09 +0200

    Exercice Openclassroom

---
+++ src/main/java/com/openclassrooms/ideinstall/com/openclassrooms/ideinstall/BasicExample.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.ideinstall.com.openclassrooms.ideinstall;
+
+public class BasicExample {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0; i<1; i++) {
+			System.out.println("Hello bugs !");
+		}
+	}
+}
