6ee2e139f4a141baa717cc46ba7e56c2e4092cd9
MoRhy DIA <morydia96@gmail.com>
2020-07-03 01:16:02 +0000

    error solved at line 7, 6

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -1,11 +1,12 @@
 package com.openclassrooms.ideinstall;
 
 public class BasicExample {
-
+//solved 
 	public static void main(String[] args) {
-		final int index;
-		for(i=0; i>=0; i++) {
-			System.out.println("Hello bugs !"):
+		int i;
+		for (i = 0; i < 10; i++) {
+			System.out.println("Hello bugs !");
+			System.out.println("Hello bugs !");
 		}
 	}
 }
