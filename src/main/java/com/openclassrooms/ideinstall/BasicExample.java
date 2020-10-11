b9de4785d875fca1d79a41b9d296965f0840fe2f
daniel.levy <daniel.levy@cerema.fr>
2020-10-11 16:08:59 +0200

    Correction des erreurs de syntaxe, de POM et d'exécution

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -1,11 +1,39 @@
 package com.openclassrooms.ideinstall;
 
+/**
+ * CLASSE BasicExample :<br/>
+ * .<br/>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author Daniel Lévy
+ * @version 1.0
+ * @since 11 oct. 2020
+ */
 public class BasicExample {
 
-	public static void main(String[] args) {
-		final int index;
-		for(i=0; i>=0; i++) {
-			System.out.println("Hello bugs !"):
+	/**
+	 * @param pArgs : String[]
+	 */
+	public static void main(final String... pArgs) {
+		
+		int i;
+		
+		for(i=0; i < 3; i++) {
+			System.out.println("Hello bugs !");
 		}
-	}
-}
+		
+	} // Fin de main(...)._________________________________________________
+	
+	
+	
+} // FIN DE LA CLASSE BasicExample.------------------------------------------
