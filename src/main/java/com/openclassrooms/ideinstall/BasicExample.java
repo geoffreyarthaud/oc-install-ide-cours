851975a230b623445fe8842151ed4ae75b82e480
Aymerick <ayce.ayki@protonmail.com>
2020-11-14 10:47:40 +0100

    Add String str Change final int index to int i Change for to remove infinity loop

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,9 +3,10 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		final int index;
-		for(i=0; i>=0; i++) {
-			System.out.println("Hello bugs !"):
+		String str = "Inutile juste test ligne - de git";
+		int i;
+		for(i = 10; i>= 0; i--) {
+			System.out.println("Hello bugs !");
 		}
 	}
 }
