2fd39c3640d930ce26067ba902bf826818c9c95a
Omar BESSAA <omarbessaa@gmail.com>
2020-11-16 22:10:45 +0100

    fix infinite loop and remove ineffectual code

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,8 +3,7 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		final int index;
-		for(int i=0; i>=0; i++) {
+		for(int i=0; i<3; i++) {
 			System.out.println("Hello bugs !");
 		}
 	}
