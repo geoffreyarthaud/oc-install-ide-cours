177a6a16a5cc43ef7c29592ec6a2f2174ec2e2c1
LENOVO <LENOVO@LENOVO-PC>
2021-03-06 18:54:23 +0100

    Changement d'operation

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		 int j;  
 		for(j=0; j< 5; j++) {
-			System.out.println("Hello bugs !");
+			System.out.println("it is ok !");
 		}
 	}
 }
