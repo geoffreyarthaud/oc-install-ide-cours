0204cca958e110784278d112088b4429a0619309
filsdelhomme <luc94camara@gmail.com>
2021-03-15 19:54:20 +0000

    CORRECTION SYNTAXIQUE ET EXECUTION

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,7 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i;
-		for (i = 0; i < 4; i++) {
+		for (i = 0; i < 5; i++) {
 			System.out.println("Hello bugs !");
 		}
 	}
