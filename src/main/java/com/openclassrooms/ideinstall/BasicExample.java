d814e0759378ed2b1cfd3ebe77abeb3adaf8aeac
guillaumebrenot <guillaumebrenot@imacdeguillaume>
2021-08-04 22:54:29 +0200

    Done and update

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,10 +5,12 @@ public class BasicExample {
 	public static void main(String[] args) {
 		
 		int i;
+		
 		for(i=0; i<3; i++) {
 			
 			 System.out.println("Hello bugs !");
-			 System.out.println(i);
+			 System.out.println("Ca fait" + i + " fois !");
+			 
 		}
 	}
 }
