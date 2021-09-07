a69c66f4b9ead2a66afcd9605639beb664277f42
IrwinWandy <90219336+IrwinWandy@users.noreply.github.com>
2021-09-07 21:56:32 +0100

    Add files via upload
    modified java code loops thrice

---
+++ BasicExample.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.ideinstall;
+
+public class BasicExample {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 2; i++) {
+			System.out.println("Hello bugs !");
+		}
+	}
+}
