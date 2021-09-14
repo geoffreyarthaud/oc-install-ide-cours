77e9c67410366c2892374c2a9c1d377e29dffd37
Brahim Neqrouz <extern.brahim.neqrouz@volkswagen.de>
2021-09-14 16:09:42 +0200

    correction

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		final int index;
 		for(i=0; i>=0; i++) {
-			System.out.println("Hello bugs !"):
+			System.out.println("Hello bugs !");
 		}
 	}
 }
