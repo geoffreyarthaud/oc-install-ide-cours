504752c4775a3d00aa15b791f6cf0e1b5e5b1f99
yoong <yoongk2000-1@yahoo.fr>
2021-11-07 18:01:07 +0100

    ss

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,7 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int i;	
+		int iss;	
 		for(i=0; i<13; i++) {
 			System.out.println("Hello bugs !");
 		}
