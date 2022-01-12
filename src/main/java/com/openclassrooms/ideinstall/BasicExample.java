47a47796334feef78808feca3d363757e144b9d6
ahmed <ahmed@ahmeds-macbook-air.home>
2022-01-12 12:06:20 +0100

    add "Mini" in println

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,8 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for (i = 0; i < 3; i++) {
-			System.out.println("Hello bugs !");
+			System.out.println("Hello mini bugs !");
+
 		}
 	}
 }
