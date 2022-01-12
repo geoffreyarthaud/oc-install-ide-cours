98af17ac07d227745f42ad64df208e7a9ee736ad
ahmed <ahmed@ahmeds-macbook-air.home>
2022-01-12 12:23:55 +0100

    add "Love" println

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for (i = 0; i < 3; i++) {
-			System.out.println("Hello mini bugs !");
+			System.out.println("Hello mini bugs ! Love");
 
 		}
 	}
