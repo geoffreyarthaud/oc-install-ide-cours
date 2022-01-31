c7bbccacf4d789ece36f91a0a2f5ee5c2c2dacc5
rapha <rapha@192.168.1.83>
2022-01-31 23:37:17 +0100

    ttt

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,7 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int index;
-		for (i = 0; i < 2; i++) {
+		for (i = 0; i < 3; i++) {
 			System.out.println("Hello bugs !");
 		}
 	}
