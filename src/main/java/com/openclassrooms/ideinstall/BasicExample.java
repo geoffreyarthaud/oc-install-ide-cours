84e0f1a9599c9e5c2a6aaee8387742ce50c2cfcb
lenovo <lenovo@DESKTOP-KQNFCLJ>
2022-03-23 11:32:05 +0100

    hello message correction des erreurs de syntaxe et des erreurs d'execution.

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,7 +4,9 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i;
+		System.out.println("hello world");
 		for (i = 0; i <= 10; i++) {
+
 			System.out.println("Hello bugs !");
 		}
 	}
