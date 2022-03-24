36a8ed026c8f486cb8eb1f2646dbec1e07e12901
mikecarmes <adrienjean@mailz.org>
2022-03-24 15:59:12 +0100

    Correction des erreurs grossières de Geoffrey

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,7 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 6; i++) {
+		for (int i = 0; i < 3; i++) {
 			System.out.println("Hello World !");
 		}
 	}
