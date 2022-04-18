b2998ffd27029dcd9e42a214f5d4c161bb1f38df
hadidi09 <cheikhhadramy@yahoo.fr>
2022-04-18 14:30:25 +0200

    Bugs fixed

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,8 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for(i=0; i < 10; i++) {
-			System.out.println("Bugs modified again !");
+			System.out.println("Bugs modified!");
+			System.out.println("It is works!");
 		}
 	}
 }
