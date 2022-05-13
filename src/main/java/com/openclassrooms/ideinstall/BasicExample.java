92d158a7eed4fb20991200ff7529d8786e5392a5
Yves <Yves@Desktop-Yves>
2022-05-13 11:12:53 -0400

    Correction des erreurs de syntaxe et d'execution

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,8 +4,9 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i;
-		for (i = 0; i < 3; i++) {
-			System.out.println("Hello bugs !");
+		for (i = 0; i < 5; i++) {
+			int num = i + 1;
+			System.out.println("Hello It's the number " + num);
 		}
 	}
 }
