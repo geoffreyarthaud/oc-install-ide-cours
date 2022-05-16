b5bd4d4533ef8dc23d287f21a41cf71504d18926
MaximeRenard <maxime.renard@protonmail.com>
2022-05-16 18:07:59 +0200

    Correction des erreurs de synthaxe

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,8 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i;
-		for(i=0; i<3; i++) {
-			//résolution erreur 
+		for(i=0; i<3; i++) { 
 			System.out.println("Hello bugs !");
 		}
 	}
