bc97386f9d9a5be493ac0984fbf65311544ba461
ssall81 <sall.sylvie@yahoo.fr>
2022-05-19 14:19:48 +0200

    Correction des erreurs de syntaxe et d'exécution

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -1,11 +1,14 @@
 package com.openclassrooms.ideinstall;
 
-public class BasicExample {
+public class BasicExample
+{
 
-	public static void main(String[] args) {
-		final int index;
-		for(i=0; i>=0; i++) {
-			System.out.println("Hello bugs !"):
-		}
-	}
+    public static void main(String[] args)
+    {
+        int index;
+        for (index = 0; index < 11; index++)
+        {
+            System.out.println("Hello bugs !");
+        }
+    }
 }
