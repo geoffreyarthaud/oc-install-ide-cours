80918d1993de4531cadbe98949458b8c4732159a
User <User@192.168.1.44>
2022-06-11 00:33:47 +0200

    correction des problemes

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,7 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int index;
-		for (index = 0; index <= 5; index++) {
+		for (index = 0; index <= 3; index++) {
 			System.out.println("Hello bugs !");
 		}
 	}
