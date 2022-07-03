df93cdcc49ad22ef63cf4d109ef5aa28ea25b64b
Lucas Frémanger <luc891@gmail.com>
2022-07-03 21:45:11 +0200

    Ajout et modification varibales

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,7 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int i;
+		int i, j, k;
 		for (i = 0; i < 3; i++) {
 			System.out.println("Hello bugs !");
 		}
