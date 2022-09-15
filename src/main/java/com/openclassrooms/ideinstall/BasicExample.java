987b8f482bc907d311f80f54d00fc152699de5ab
One1Sun <suntoutcourt@gmail.com>
2022-09-15 16:31:18 +0200

    Correction syntaxe

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for(i=0; i<4; i++) {
-			System.out.println("Hello no bugs now !");
+			System.out.println("Hello no bugs now!");
 		}
 	}
 }
