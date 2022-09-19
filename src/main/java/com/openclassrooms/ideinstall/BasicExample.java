c37263f8353007a8339dae8bedcea4fe73ad1157
trimok <trimok@mail.fr>
2022-09-19 15:04:26 +0200

    2eme commit on branch dev

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,7 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i;
-		for (i = 0; i < 12; i++) {
+		for (i = 0; i < 13; i++) {
 			System.out.println("Hello bugs !");
 		}
 	}
