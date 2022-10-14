3445f10ebd1f8aa2e90b9eb866ab507c7a66c357
ebevazaha <ebevazaha@ITEM-S136584.emea.msad.sopra>
2022-10-14 15:27:03 +0200

    autre modif

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for (i = 0; i < 3; i++) {
-			System.out.println("Hell bugs !");
+			System.out.println("Hell ugs !");
 		}
 	}
 }
