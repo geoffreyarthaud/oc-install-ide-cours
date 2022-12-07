d6132e7f8667f5d2da4417518322bd0c02a4f5fb
Tovoasina <Tovoasina@DESKTOP-O4VP3ND>
2022-12-07 17:05:41 +0100

    Test 4 after generating Git Token

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int index;
 		for( index=0; index<=10; index++) {
-			System.out.println("Hello bugs 3!");
+			System.out.println("Hello bugs 4!");
 		}
 	}
 }
