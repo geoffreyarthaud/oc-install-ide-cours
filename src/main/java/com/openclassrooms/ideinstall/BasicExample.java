12258054e6386873b8d7530be628b214b26c4536
ramzy <ramzy@DESKTOP-ASBTT8H>
2022-12-31 23:56:03 +0100

    hghghgh

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,8 +3,8 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int i;
-		for(i=0; i< 20; i++) {
+		int j;
+		for(j=0; j< 20; j++) {
 			System.out.println("Hello bugs !");
 		}
 	}
