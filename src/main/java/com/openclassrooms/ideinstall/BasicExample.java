f5c2aa49cbc392e5b952a2230ca1c3729fb3ada0
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2023-02-13 17:06:36 +0100

    zerzer

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,10 +3,10 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int i;
-		for(i=0; i<=7; i++) {
+		int i=0;
+		for(i=0; i<=4; i++) {
 			System.out.println("Hello bugs !");
-			//commentaire qui sert a rien pour un test de push github
+			//commentaire qurereri sert a rien pour un test de push github
 		}
 	}
 }
