64d1b08bc38fcd6519fd980f45a36c9b45bd1858
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2023-02-13 16:48:40 +0100

    test de synchro github

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,8 +4,9 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i;
-		for(i=0; i<=8; i++) {
+		for(i=0; i<=7; i++) {
 			System.out.println("Hello bugs !");
+			//commentaire qui sert a rien pour un test de push github
 		}
 	}
 }
