957867433710820e7d682b52a7592deda9c0e072
Fouad <Fouad@DESKTOP-KFULNDQ.lan>
2023-02-27 23:11:16 +0100

    Correction 2

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for(i=0; i<3; i++) {
-			System.out.println("Hello bugs !");
+			System.out.println("Hello bugs !!!");
 		}
 	}
 }
