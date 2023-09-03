a1849634b496ecdf8514e195d4ee28ddc4dadd7c
dubro <dubro@DESKTOP-B1K2HSJ.lan>
2023-09-03 18:49:15 +0200

    debuging errors

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for (i = 0; i <= 10; i++) {
-			System.out.println("Hello bugs !");
+			System.out.println("Hello bugs !!!!");
 		}
 	}
 }
