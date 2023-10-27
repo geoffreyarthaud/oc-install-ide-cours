e8eae66e72fd4d1b5081daf844292d09faeef2a2
kim90 <manfalouti.kamal@gmail.com>
2023-10-27 11:35:13 +0200

    ajout System.out.println("Hello bugs !5");

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -6,10 +6,11 @@ public class BasicExample {
 	    int i;
 		for(i=0; i<3; i++) {
 			System.out.println("Hello bugs !");
-			System.out.println("Hello bugs !:)");
-			System.out.println("Hello bugs !:):)");
-			System.out.println("Hello bugs !:):):)");
-			System.out.println("Hello bugs !:):):):)");
+			System.out.println("Hello bugs !1");
+			System.out.println("Hello bugs !2");
+			System.out.println("Hello bugs !3");
+			System.out.println("Hello bugs !4");
+			System.out.println("Hello bugs !5");
 		}
 	}
 }
