1b52af4d2bfc51eeb0a51006c40998a3e8ce1b4e
Inoussakere <88610756+Inoussakere@users.noreply.github.com>
2024-01-05 01:31:57 +0100

    Tentative commit and push

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,6 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for (i = 0; i <= 10; i++) {
+			// affichage de message
 			System.out.println("Hello bugs !");
 		}
 	}
