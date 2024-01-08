a0a93b97a1b5f23e31e9f8108befec7bc959693f
Inoussakere <88610756+Inoussakere@users.noreply.github.com>
2024-01-08 11:56:22 +0100

    Correction de erreurs + ajout d'un commentaire.

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,9 +3,10 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		final int index;
-		for(i=0; i>=0; i++) {
-			System.out.println("Hello bugs !"):
+		int i;
+		for (i = 0; i < 5; i++) {
+			// affichage du message 5 fois
+			System.out.println("N°" + (i + 1) + " - Hello bugs !");
 		}
 	}
 }
