52716e4bd005fd432799734095d50a3de9e2e0c2
HENRI <HENRI@192.168.1.135>
2024-02-29 14:49:00 +0000

    Correction syntaxe et bug

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int i;
 		for (i = 0; i < 3; i++) {
-			System.out.println("Hello bugss !");
+			System.out.println("Hello bugsss !");
 		}
 	}
 }
